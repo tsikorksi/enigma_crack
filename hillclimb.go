221928f09cce86f56ef5d7e7cc3e27f09778869b
tsikorksi <tsikors2@jh.edu>
2021-10-25 16:56:29 -0400

    Final update, brought it up to submitted version

--- hillclimb.go
+++ hillclimb.go
@@ -12,85 +12,25 @@ import (
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+var trisMap map[string]int64
+
+// Generates Trigram score lookup table
+// Trigrams sourced from:
+// http://practicalcryptography.com/cryptanalysis/letter-frequencies-various-languages/english-letter-frequencies/
+func processGrams(tris string) map[string]int64 {
+	trisMap = make(map[string]int64)
+	trisArray := strings.Split(tris, "\n")
+	for _, line := range trisArray {
+		lineData := strings.Split(line, " ")
+		trisMap[lineData[0]], _ = strconv.ParseInt(strings.Trim(lineData[1], "\r"), 10, 32)
+	}
+	return trisMap
+}
+
 //const englishIOC = 0.06577359255736807
 
 // IOC of english text, calculated using one time script from the corpus 'The Count of Monte Cristo'
 // 0.06577359255736807
-
-//IC on rotors and config -> IC on plugboard -> trigram on plugboard
-
-// Swaps two letters in ciphertext and checks the IoC for the new version
-func singleSwap(content string, config []enigma.RotorConfig, swapList []string) float64 {
-	return math.Round(calcIC(enigmaSimulate(config[0], config[1], content, swapList))*10000) / 10000
-}
-
-// Finds all legal swaps and their IoC after such a swap is made
-func findSwaps(content string, config []enigma.RotorConfig) map[string]float64 {
-	var initial = math.Round(calcIC(content)*10000) / 10000
-	var scoreMap = make(map[string]float64)
-	var swapList = make([]string, 0, 10)
-	var score float64
-	for _, letter := range alphabet {
-		for _, letter2 := range alphabet {
-			// Because AB = BA ...
-			s := strings.Split(string(letter)+string(letter2), "")
-			sort.Strings(s)
-			var joined = strings.Join(s, "")
-			_, k := scoreMap[joined]
-			// Because AA etc. is not possible...
-			if letter != letter2 || k {
-
-				if strings.ContainsAny(strings.Join(swapList, ""), joined) {
-					for index, value := range swapList {
-						if strings.Contains(value, string(letter2)) {
-							swapList = append(swapList[:index], swapList[index+1:]...)
-						}
-					}
-					swapList = append(swapList, string(letter)+string(letter))
-					score = singleSwap(content, config, swapList)
-
-					swapList = swapList[:len(swapList)-1]
-					var altScore = singleSwap(content, config, swapList)
-
-				}
-				swapList = append(swapList, joined)
-				score = singleSwap(content, config, swapList)
-				if score > initial {
-					scoreMap[joined] = score
-					initial = score
-				}
-
-			}
-		}
-	}
-
-	return scoreMap
-}
-
-// removes worse performing 2/3rds of possible swaps
-func extractBetter(swaps map[string]float64) map[string]float64 {
-	var total float64 = 0
-	var values []float64
-	for _, val := range swaps {
-		total += val
-		values = append(values, val)
-	}
-	var avg = total / float64(len(swaps))
-
-	var xTotal float64
-	for _, x := range values {
-		xTotal += math.Pow(x-avg, 2)
-	}
-
-	for key, value := range swaps {
-		//+ (0.25 * math.Sqrt(xTotal/float64(len(swaps))))
-		if value < avg {
-			delete(swaps, key)
-		}
-	}
-	return swaps
-}
-
 // Calculates IoC
 func calcIC(text string) float64 {
 	n := float64(len(text))
@@ -102,27 +42,109 @@ func calcIC(text string) float64 {
 	return sum / (n * (n - 1))
 }
 
-// Generates Trigram score lookup table
-// Trigrams sourced from:
-// http://practicalcryptography.com/cryptanalysis/letter-frequencies-various-languages/english-letter-frequencies/
-func processGrams(tris string) map[string]int64 {
-	var trisMap = make(map[string]int64)
-	trisArray := strings.Split(tris, "\n")
-	for _, line := range trisArray {
-		lineData := strings.Split(line, " ")
-		trisMap[lineData[0]], _ = strconv.ParseInt(strings.Trim(lineData[1], "\r"), 10, 32)
-	}
-	return trisMap
-}
-
 // Generates Trigram score for content from pre-generated trigram map
-func gramScore(trisMap map[string]int64, content string) int64 {
+func gramScore(content string) int64 {
 	var score int64 = 0
 	for key, value := range trisMap {
 		var count = int64(strings.Count(content, key))
 		score += count * value
 	}
 	return score
+}
+
+// Swaps two letters in ciphertext and checks the IoC for the new version
+func singleSwapIOC(content string, config []enigma.RotorConfig, swapList []string) float64 {
+	return math.Round(calcIC(enigmaSimulate(config[0], config[1], content, swapList))*10000) / 10000
+}
+
+// The lack of generics is literally killing me
+func singleSwapGram(content string, config []enigma.RotorConfig, swapList []string) int64 {
+	return gramScore(enigmaSimulate(config[0], config[1], content, swapList))
+}
+
+// Small wrapper on standard swap function
+func conductSwapIOC(swapList []string, content string, config []enigma.RotorConfig, joined string, i int) []string {
+	var initial = singleSwapIOC(content, config, swapList)
+	var temp = swapList[i]
+	swapList[i] = joined
+	var score = singleSwapIOC(content, config, swapList)
+	if score < initial {
+		swapList[i] = temp
+	}
+	return swapList
+}
+
+//// Small wrapper on standard swap function, but with grams
+//func conductSwapGram(swapList []string, content string, config []enigma.RotorConfig, joined string, i int) []string {
+//	var initial = singleSwapGram(content, config, swapList)
+//	var temp = swapList[i]
+//	swapList[i] = joined
+//	var score = singleSwapGram(content, config, swapList)
+//	if score < initial {
+//		swapList[i] = temp
+//	}
+//	return swapList
+//}
+
+// Finds all legal swaps and their IoC after such a swap is made
+func hillclimbIOC(content string, config []enigma.RotorConfig, swapList []string) ([]string, map[string]float64) {
+	var scoreMap = make(map[string]float64)
+	for _, letter := range alphabet {
+		for _, letter2 := range alphabet {
+			var swapped = false
+			// Because AB = BA ...
+			s := strings.Split(string(letter)+string(letter2), "")
+			sort.Strings(s)
+			var joined = strings.Join(s, "")
+			_, ok := scoreMap[joined]
+			// Because AA etc. is not possible...
+			if letter != letter2 && !ok {
+				scoreMap[joined] = 0.0
+				if len(swapList) == 0 {
+					swapList = append(swapList, joined)
+					swapped = true
+				} else if !swapped && strings.ContainsAny(strings.Join(swapList, ""), string(letter)) && strings.ContainsAny(strings.Join(swapList, ""), string(letter2)) {
+					var initial = singleSwapIOC(content, config, swapList)
+					var tempSwaps []string
+					for i, v := range swapList {
+						if strings.ContainsAny(v, joined) {
+							tempSwaps = append(tempSwaps, v)
+							swapList[i] = ""
+						}
+					}
+					sort.Strings(swapList)
+					swapList = swapList[2:]
+					swapList = append(swapList, joined)
+					var score = singleSwapIOC(content, config, swapList)
+
+					if initial > score {
+						swapList = swapList[:len(swapList)-1]
+						swapList = append(swapList, tempSwaps...)
+					}
+					sort.Strings(swapList)
+				} else {
+					for i, v := range swapList {
+						if string(v[0]) == string(joined[0]) && !strings.ContainsAny(strings.Join(swapList, ""), string(letter2)) {
+							conductSwapIOC(swapList, content, config, joined, i)
+							swapped = true
+							break
+						} else if string(v[1]) == string(joined[1]) && !swapped && !strings.ContainsAny(strings.Join(swapList, ""), string(letter)) {
+							conductSwapIOC(swapList, content, config, joined, i)
+							swapped = true
+						} else if !strings.ContainsAny(strings.Join(swapList, ""), joined) {
+							swapList = append(swapList, joined)
+							swapped = true
+						}
+						swapped = false
+
+					}
+				}
+
+			}
+		}
+	}
+
+	return swapList[:10], scoreMap
 }
 
 type Pair struct {
@@ -140,23 +162,22 @@ func (p PairList) Len() int           { return len(p) }
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
 
-func trySwaps(swaps map[string]float64, config []enigma.RotorConfig, trisMap map[string]int64, content string) []string {
+func hillclimbTrigram(swaps map[string]float64, config []enigma.RotorConfig, content string, plugboard []string) []string {
 	var finalSwaps []string
 	for key := range swaps {
-		finalSwaps = append(finalSwaps, key)
+		if !strings.ContainsAny(strings.Join(plugboard, ""), key) {
+			finalSwaps = append(finalSwaps, key)
+		}
 	}
 	//var plugboard = finalSwaps[0:10]
 	//var score = gramScore(trisMap, enigmaSimulate(config[0], config[1], content, []string{}))
 	var plugboardMap = make(map[string]int64)
 
-	//var combos = combinations.Combinations(finalSwaps, 10)
 	for _, combo := range finalSwaps {
-		var tempSwap = []string{combo}
-		var tempScore = gramScore(trisMap, enigmaSimulate(config[0], config[1], content, tempSwap))
-		plugboardMap[tempSwap[0]] = tempScore
-	}
+		var tempScore = singleSwapGram(content, config, append(plugboard, combo))
+		plugboardMap[combo] = tempScore
 
-	var plugboard = make([]string, 0, 10)
+	}
 
 	var pairList PairList
 
@@ -164,10 +185,10 @@ func trySwaps(swaps map[string]float64, config []enigma.RotorConfig, trisMap map
 		pairList = append(pairList, Pair{key, value})
 	}
 	sort.Sort(sort.Reverse(pairList))
-	var count = 0
+	var count = 7
 	// remove duplicate letter swaps, selecting best
 	for len(plugboard) < 10 {
-		if !strings.ContainsAny(strings.Join(plugboard, ""), pairList[count].Key) {
+		if !strings.ContainsAny(strings.Join(plugboard[7:], ""), pairList[count].Key) {
 			plugboard = append(plugboard, pairList[count].Key)
 		}
 		count++
@@ -264,23 +285,26 @@ func main() {
 	content := readFile("ct.txt")
 	content = enigma.SanitizePlaintext(content)
 	tris := readFile("english_tri.txt")
-	var trisMap = processGrams(tris)
+	trisMap = processGrams(tris)
 	var config = rotorBrute(content)
-	var swaps = extractBetter(findSwaps(content, config))
-	var plugboard = trySwaps(swaps, config, trisMap, content)
+	var swapList = make([]string, 0, 48)
+	//var scoreMap map[string]float64
+	plugboard, _ := hillclimbIOC(content, config, swapList)
+	//var plugboard = make([]string, 0, 10)
+	//plugboard = hillclimbTrigram(scoreMap ,config, content, swapList)
 	sort.Strings(plugboard)
 
+	var id []string
 	for _, v := range config {
-		fmt.Print(v.ID + " ")
+		id = append(id, v.ID)
 	}
-	fmt.Println()
+	fmt.Println(strings.Join(id, " "))
+	var starts []string
 	for _, v := range config {
-		fmt.Print(string(v.Start) + " ")
+		starts = append(starts, string(v.Start))
 	}
-	fmt.Println()
-	for _, v := range plugboard {
-		fmt.Print(string(v) + " ")
-	}
-	fmt.Println()
-	fmt.Println(enigmaSimulate(config[0], config[1], content, plugboard))
+	fmt.Println(strings.Join(starts, " "))
+
+	fmt.Println(strings.Join(plugboard, " "))
+	//fmt.Print(enigmaSimulate(config[0], config[1], content, plugboard))
 }
